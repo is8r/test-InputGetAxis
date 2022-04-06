1cefc23c30da7809e5e64f9f63477d1408c081ba
Yu Ishihara <ishihara.yu@gmail.com>
2022-04-07 00:26:43 +0900

    GetAxisRawを追加

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,16 +6,21 @@ using UnityEngine.UI;
 public class Test : MonoBehaviour
 {
     [SerializeField] Text log;
+    [SerializeField] Text log2;
 
     void Start()
     {
-        Application.targetFrameRate = 10;
+        Application.targetFrameRate = 5;
     }
 
     void Update()
     {
+        float horizontalRaw = Input.GetAxisRaw("Horizontal");
+        float verticalRaw = Input.GetAxisRaw("Vertical");
+        log.text = $"horizontalRaw: {horizontalRaw}, verticalRaw: {verticalRaw}";
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        log.text = $"horizontal: {horizontal}, vertical: {vertical}";
+        log2.text = $"horizontal: {horizontal}, vertical: {vertical}";
     }
 }
